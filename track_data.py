8056da872c4ad3d65a7825fdd2764c7168036303
Alex Lyttle <a.j.lyttle@outlook.com>
2019-07-29 17:17:46 +0100

    Restructured track_data.py.

--- track_data.py
+++ track_data.py
@@ -3,22 +3,18 @@
 
 from pandas import DataFrame, read_csv
 
-class TrackData:
-    """Structure containing DataFrames from a MESA track history output file.
 
-    Reads a history file assuming the following default structure:
+class MesaData:
+    """Structure containing DataFrames from a MESA output file.
+
+    Reads an output file assuming the following default structure:
     - line 0: header names
     - line 1: header data
     - line 2: blank (ignored by pandas.read_csv)
     - line 3: main data names
     - line 4: main data values
-    but may be altered via class methods TrackData.set_header_rows and 
-    TrackData.set_data_rows.
-
-    Parameters
-    ----------
-    file_name : str, optional
-        File name to be read in. The default is 'LOGS/history.data'. 
+    but may be altered via class methods MesaData.set_header_rows and 
+    MesaData.set_data_rows.
 
     Attributes
     ----------
@@ -28,7 +24,6 @@ class TrackData:
         The main data from the MESA history track.
     header : pandas.DataFrame
         The header data from the MESA history track.
-    ***ADD SECTION ON INITIAL ATTRIBUTES***
     """
 
     header_line = 1
@@ -41,64 +36,140 @@ class TrackData:
     @classmethod
     def set_data_rows(cls, line=4):
         cls.data_line = line
-
-    def __init__(self, file_name='LOGS/history.data'):
+    
+    def __init__(self, file_name='LOGS/history.data', read_file=True,
+                 data=None, header=None):
         self.file_name = file_name
-        self.data = DataFrame()
-        self.header = DataFrame()
-        self.initial_mass = None  
-        self.initial_z = None
-        self.read_data()  # Reads data in the file 'self.file_name'
+        self.data = data
+        self.header = header
+        if read_file:
+            self.read_data()
 
     def __repr__(self):
-        # ***ADD INITIAL_MASS AND INITIAL_Z ATTRIBUTES***
-        return 'TrackData(file_name={0}, data={1}, header={2})'.format(
+        return 'MesaData(file_name={0}, data={1}, header={2})'.format(
             self.file_name, self.data, self.header)
 
     def __str__(self):
-        return ('A member of TrackData with attributes,\n\n' +
+        return ('A member of MesaData with attributes,\n\n' +
                 'file_name:\n"{}"\n\n'.format(self.file_name) +
                 'data:\n{}\n\n'.format(self.data) +
                 'header:\n{}'.format(self.header)
                 )
-
+    
     def read_data(self):
         """Loads or updates track data into a series of pandas DataFrame 
         objects.
 
         This reads the data from the file name provided. Lots of error checks
         needed here!
-
         """
         self.header = read_csv(self.file_name, delim_whitespace=True,
                                header=TrackData.header_line, nrows=1)
         self.data = read_csv(self.file_name, delim_whitespace=True, 
                              header=TrackData.data_line)
-        self.initial_mass = self.header.initial_mass[0]
-        self.initial_z = self.header.initial_z[0]
-        
-    def crop_rc(self, evo_state, center_he4_upper=0.95, center_he4_lower=1e-4,
-                center_c12_lower=0.05):
+
+
+class TrackData(MesaData):
+    """Structure containing DataFrames from a MESA track history output file.
+
+    Attributes
+    ----------
+    file_name : str
+        Path to the file from which the data is read.
+    data : pandas.DataFrame
+        The main data from the MESA history track.
+    header : pandas.DataFrame
+        The header data from the MESA history track.
+    initial_mass : float
+        The initial mass of the stellar track.
+    initial_z : float
+        The initial metallicity of the stellar track
+    """
+
+    def __init__(self, file_name='LOGS/history.data', read_file=True,
+                 data=None, header=None, initial_mass=None, initial_z=None):
+
+        MesaData.__init__(self, file_name=file_name, read_file=read_file,
+                          data=data, header=header)
+        self.set_initial_conditions(initial_mass=initial_mass,
+                                      initial_z=initial_z)
+
+    def __repr__(self):
+        return ('TrackData(file_name={0}, data={1}, header={2}, '.format(
+                self.file_name, self.data, self.header) +
+                'initial_mass={0}, initial_z={1}'.format(
+                self.initial_mass, self.initial_z))
+
+    def __str__(self):
+        return ('A member of TrackData with attributes,\n\n' +
+                'file_name:\n"{}"\n\n'.format(self.file_name) +
+                'data:\n{}\n\n'.format(self.data) +
+                'header:\n{}'.format(self.header) +
+                'initial_mass:\n{}'.format(self.initial_mass) +
+                'initial_z:\n{}'.format(self.initial_z)
+                )
+    
+    def set_initial_conditions(self, initial_mass=None, initial_z=None):
+        """Sets initial conditions of the stellar track to what is
+        provided in the header data (default) unless
+        provided explicitly in the keyword arguements.
+
+        Attributes
+        ----------
+        initial_mass : float, optional
+            Initial mass of the evolutionary track.
+        initial_z : float, optional    
+            Initial metallicity of the evolutionary track.
         """
-        Trims a given track history DataFrame subject to He core-burning conditions to produce a track in the red clump.
-        The initial he4 is at 0.95 - Z, initial c12 is at 0.05 and track ends with he4 at 1e-4.
-        The upper limit is, central He < (0.95 - Z).
+        self.initial_mass = initial_mass if initial_mass else \
+            self.header.initial_mass[0]
+        self.initial_z = initial_z if initial_z else self.header.initial_z[0]
+
+    def crop_rc(self, center_he4_upper=0.95, center_he4_lower=1e-4,
+                center_c12_lower=0.05):
+        """Trims a given TrackData DataFrame subject to core helium-burning 
+        conditions to produce a track in the red clump (or horizontal branch).
+        
+        The start of the core helium-burning phase is defined by either an
+        upper limit on central helium - default is (0.95 - initial_z) - or
+        a lower limit on central carbon - default is 0.05. These are chosen
+        so as to bypass the helium flash.
+        
+        The end of the core helium-burning phase is defined by a lower limit on
+        central helium - default is 1e-4.
+
+        Attributes
+        ----------
+        center_he4_upper : float, optional
+            The upper limit on the central helium fraction minus the
+            initial metallicity.
+        center_he4_lower : float, optional
+            The lower limit on the central helium fraction.
+        center_c12_lower : float, optional
+            The lower limit on the central carbon fraction.
         """
         if self.data.empty:
-            # If data is empty, an empty DataFrame is returned
-            cropped_data = DataFrame()
-            print('Note, empty input DataFrame provided - nothing to crop.')
+            # If data is empty, the original track object is returned
+            cropped_track = self
+            print('Note, empty input data provided - nothing to crop.')
         else:
             condition = (self.data['center_he4'] < 
                          center_he4_upper - self.initial_z) &\
                         (self.data['center_he4'] > center_he4_lower) &\
                         (self.data['center_c12'] > center_c12_lower)
             cropped_data = self.data.loc[condition]
-            cropped_data = cropped_data.reset_index()
-            # Note - could have this apply the crop to itself? This may need
-            # an additional function for changing self.data to a new DataFrame
+            cropped_data = cropped_data.reset_index(drop=True)
+            cropped_track = self  # Copy self into new, cropped track.
+            cropped_track.data = cropped_data
+            # Isn't this better as a static method? Or just make the change
+            # to the current object?
+        
+        return cropped_track
 
-        return cropped_data
+
+class ProfileData(MesaData):
+    """Structure containing DataFrames from a MESA profile output file. TBC.
+    """
 
 
 if __name__ == '__main__':
--- track_interpolator.py
+++ track_interpolator.py
@@ -4,4 +4,30 @@
 # interpolates in age, mass and metallicity given certain tolerences.
 # It could be in the form of a DataFrame with columns for age, mass and 
 # metallicity, or it could be a multidimentionaly numpy array (whichever is 
-# easier to sample from - probably the latter). It 
+# easier to sample from - probably the latter).
+
+# Use scipy.interpolate.griddata with linear or cubic method where points is
+# the set of ages, masses and metallicities available and values are the 
+# numpy.arrays of column values for each one
+
+class TrackInterpolator:
+    """Contains methods for interpolating a grid of TrackData objects.
+
+    Need a grid of TrackData objects 
+    """
+    
+    def __init__(self):
+        return None
+    
+    def norm_age(self, track_data):
+        min_age = min(track_data['star_age'])
+        max_age = max(track_data['star_age'])
+        track_data['norm_age'] = (track_data.loc[:, 'star_age'] - min_age) /\
+            (max_age - min_age)
+    
+    def  sort_grid(self, tracks, masses, metallicities):
+        x = [[[]]]
+        sorted_tracks = [[[]]]
+        for i in range(len(metallicities)):
+            for j in masses:
+                sorted_tracks.append(tracks[i][j])
